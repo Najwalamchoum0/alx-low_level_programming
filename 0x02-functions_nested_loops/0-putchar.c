5d07f06ed46297a40b3fbeae93511d41cb75e671
najwa <najwanaajwa66@gmail.com>
2023-06-21 13:31:07 -0700

    sun

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- 
-main - print _putchar
-is written by mbah
-Return: 0
-*/
+ * main - print _putchar
+   is written by mbah
+ * Return: 0
+ */
 int main(void)
 {
 putchar('');
@@ -16,5 +15,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
